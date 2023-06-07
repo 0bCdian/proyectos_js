ab9ba848165271aa2458d46ce1f7ca54cddd8f15
0bCdian <diegomiguelp1996@gmail.com>
2023-06-08 00:52:24 +0200

    challenge completed, added button to create new counters!

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
-const decrease = document.getElementById('decrease')
-const reset = document.getElementById('reset')
-const increase = document.getElementById('increase')
-const counter = document.getElementById('counter')
-let counterValue = 0
+const createCounter = document.getElementById('createCounter')
+const container = document.getElementById('container')
+let counterInstance = 0
+let value = {}
 
-decrease.addEventListener('click', () => {
-  counterValue--
-  counter.innerHTML = counterValue
-})
+function createNewCounter() {
+  const counter = document.createElement('p')
+  const decrease = document.createElement('button')
+  const increase = document.createElement('button')
+  const reset = document.createElement('button')
+  const counterId = 'value' + counterInstance
+  value[counterId] = 0
+  counter.innerHTML = value[counterId]
+  decrease.innerText = 'decrease'
+  increase.innerText = 'increase'
+  reset.innerText = 'reset'
+  counter.setAttribute('id', counterId)
+  decrease.setAttribute('class', 'decrease')
+  increase.setAttribute('class', 'increase')
+  reset.setAttribute('class', 'reset')
+  decrease.addEventListener('click', () => {
+    const counter = document.getElementById(counterId)
+    value[counterId]--
+    counter.innerHTML = value[counterId]
+  })
+  increase.addEventListener('click', () => {
+    const counter = document.getElementById(counterId)
+    value[counterId]++
+    counter.innerHTML = value[counterId]
+  })
+  reset.addEventListener('click', () => {
+    const counter = document.getElementById(counterId)
+    value[counterId] = 0
+    counter.innerHTML = value[counterId]
+  })
+  container.append(counter, decrease, reset, increase)
+  counterInstance++
+}
 
-increase.addEventListener('click', () => {
-  counterValue++
-  counter.innerHTML = counterValue
-})
-
-reset.addEventListener('click', () => {
-  counterValue = 0
-  counter.innerHTML = counterValue
-})
+createCounter.addEventListener('click', createNewCounter)
